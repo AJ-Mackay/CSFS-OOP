aa473c439e5007bc0833aa31ffc0f2765fe79ee0
Ruiner85 <paul.m.mackay85@gmail.com>
2020-08-17 15:43:35 +0100

    initial commit

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classes
+{
+    class Employee
+    {
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Dave = new Employee();
+            Dave.Age = 35;
+            Console.WriteLine("Dave's age is {0}", age);
+        }
+    }
+}
